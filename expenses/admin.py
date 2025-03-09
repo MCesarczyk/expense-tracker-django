bd8e2923db9008551bfcb96b78e5d58119c6bd80
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-03-09 15:33:21 +0100

    register entities in django admin panel

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Category, Expense, Subcategory, TransactionType
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at", "updated_at"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "created_at", "updated_at"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(TransactionType)
+class TransactionTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "transaction_date",
+        "accounting_date",
+        "transaction_type",
+        "category",
+    ]
+    search_fields = ["title"]
+    prepopulated_fields = {"slug": ("title",)}
