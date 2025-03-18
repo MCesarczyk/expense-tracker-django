bd46394dfc1627af2c37e8b92e72cab613d9e8bf
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2025-03-18 18:14:58 +0100

    feat: display data in website (#1)
    * feat: create expense list view with template
    
    * add home view and list views for all entities
    
    * add expense details view

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from expenses import models
 
-# Create your views here.
+
+def expense_all(request):
+    expenses = models.Expense.expenses.all()[:50]
+    context = {"expenses": expenses}
+    return render(request, "expenses/expenses_list.html", context)
+
+
+def expense_detail(request, slug):
+    expense = get_object_or_404(models.Expense, id=slug)
+    return render(request, "expenses/expense_detail.html", {"expense": expense})
+
+
+def category_all(request):
+    categories = models.Category.objects.all()
+    context = {"categories": categories}
+    return render(request, "expenses/categories_list.html", context)
+
+
+def subcategory_all(request):
+    subcategories = models.Subcategory.objects.all()
+    context = {"subcategories": subcategories}
+    return render(request, "expenses/subcategories_list.html", context)
+
+
+def transaction_type_all(request):
+    transaction_types = models.TransactionType.objects.all()
+    context = {"transaction_types": transaction_types}
+    return render(request, "expenses/transaction_types_list.html", context)
