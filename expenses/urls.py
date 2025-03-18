273b36bfd708871119df855f608539689271dd42
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-03-18 09:39:31 +0100

    add home view and list views for all entities

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'expenses'
 
 urlpatterns = [
-    path('', views.expense_all, name='home'),
+    path('', views.expense_all, name='expense_all'),
+    path('categories/', views.category_all, name='categories'),
+    path('subcategories/', views.subcategory_all, name='subcategories'),
+    path('transaction-types/', views.transaction_type_all, name='transaction_types'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -6,3 +6,21 @@ def expense_all(request):
     expenses = models.Expense.expenses.all()[:50]
     context = {"expenses": expenses}
     return render(request, "expenses/expenses_list.html", context)
+
+
+def category_all(request):
+    categories = models.Category.objects.all()
+    context = {"categories": categories}
+    return render(request, "expenses/categories_list.html", context)
+
+
+def subcategory_all(request):
+    subcategories = models.Subcategory.objects.all()
+    context = {"subcategories": subcategories}
+    return render(request, "expenses/subcategories_list.html", context)
+
+
+def transaction_type_all(request):
+    transaction_types = models.TransactionType.objects.all()
+    context = {"transaction_types": transaction_types}
+    return render(request, "expenses/transaction_types_list.html", context)
