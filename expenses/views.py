78fed3a1e200ca09be16c23f4faab9c85e6d3cf8
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-03-18 08:44:47 +0100

    feat: create expense list view with template

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from expenses import models
 
-# Create your views here.
+
+def expense_all(request):
+    expenses = models.Expense.expenses.all()[:50]
+    context = {"expenses": expenses}
+    return render(request, "expenses/expenses_list.html", context)
